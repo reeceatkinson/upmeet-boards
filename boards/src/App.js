cea9c51f160f7a2ef9f411d90df19454b99041eb
Reece Atkinson <hello@reeceatkinson.com>
2022-10-07 17:45:37 +0000

    Go

---
+++ boards/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Tldraw } from '@tldraw/tldraw'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Tldraw></Tldraw>
+    </div>
+  );
+}
+
+export default App;
